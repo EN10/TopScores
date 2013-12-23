4e9da5fa6a397387a0befc46d5085beb1b0ff2f0
EN10 <sfm.neo@gmail.com>
2013-12-23 23:03:43 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,14 +1,1 @@
-try 
-{ var url = require('url');
-  var url_parts = url.parse(request.url, true);
-  var query = url_parts.query;
-  
-  var fs = require('fs');
-  var filename = 'Scores.txt';
-
-  var txt = fs.readFileSync(filename).toString(); 
-  document.getElementById("main").innerHTML = txt;
-  console.log("url parts"+query);
-}
-catch(err)
-{ console.log(err); }
+console.log("Hello World");
