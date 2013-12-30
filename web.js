8cdc42e214b2152cd486688621bb50af7710377c
EN10 <sfm.neo@gmail.com>
2013-12-30 23:25:38 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  response.send(fs.readFileSync('Scores.txt').toString());
-  response.send(req.query.q);
+  // var fs = require('fs');
+  // response.send(fs.readFileSync('Scores.txt').toString());
+  response.send(req.query.q.toString());
+  console.log(req.query.toString());
 });
 
 var port = process.env.PORT || 5000;
