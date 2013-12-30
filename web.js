8cb55bd5d25e5888cc088a0b11d4b7e58f8a6d4b
EN10 <sfm.neo@gmail.com>
2013-12-30 20:01:11 +0000

    Update web.js

--- web.js
+++ web.js
@@ -15,8 +15,9 @@
  app.get(/^(.+)$/, function(req, res){ 
      console.log('static file request : ' + req.params);
      res.sendfile(__dirname + req.params[0]);
-     console.log("req para" + req.params[0]);
  });
+
+ console.log("req para" + req.params[0]);
 
  var port = process.env.PORT || 80;
  app.listen(port, function() {
