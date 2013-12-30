5394939429fb32b7ac29c5c949d3d767a434e700
EN10 <sfm.neo@gmail.com>
2013-12-30 23:18:06 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,25 +1,14 @@
- var express = require("express");
- var app = express();
+var express = require('express');
+var app = express();
+app.use(express.logger());
 
- /* serves main page */
- app.get("./", function(req, res) {
-    // res.sendfile('index.html')
+app.get('/', function(request, response) {
+  // response.send('Hello World!');
   var fs = require('fs');
   response.send(fs.readFileSync('Scores.txt').toString());
- });
+});
 
-  app.post("/user/add", function(req, res) { 
-	/* some server side logic */
-	res.send("OK");
-  });
-
- /* serves all the static files */
- app.get(/^(.+)$/, function(req, res){ 
-     console.log('static file request : ' + req.params);
-     res.sendfile(__dirname + req.params[0]);
- });
-
- var port = process.env.PORT || 80;
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
