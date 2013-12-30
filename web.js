ae14f5d2eb3b5e344589c14e5c9e2ad7f1e4c6c1
EN10 <sfm.neo@gmail.com>
2013-12-30 19:50:20 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+ var express = require("express");
+ var app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+ /* serves main page */
+ app.get("./", function(req, res) {
+    res.sendfile('index.html')
+ });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+  app.post("/user/add", function(req, res) { 
+	/* some server side logic */
+	res.send("OK");
+  });
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+     res.sendfile(__dirname + req.params[0]); 
+ });
+
+ var port = process.env.PORT || 80;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
