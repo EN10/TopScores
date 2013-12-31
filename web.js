50dca09427442b8e079896aa40e5094dcbe5e3d0
EN10 <sfm.neo@gmail.com>
2013-12-31 03:22:02 +0000

    Update web.js

--- web.js
+++ web.js
@@ -4,15 +4,11 @@ app.use(express.logger());
 
 app.get('/', function(req, res) {
   var fs = require('fs');
-  try
-{ if (fs.exists('Scores.txt'))
+  if (fs.exists('Scores.txt'))
   { if (req.query.q != undefined)
     { fs.appendFileSync('Scores.txt', "<br>" + req.query.q); }
     res.send(fs.readFileSync('Scores.txt').toString());
   }
-}    
-  catch(err)
-  { res.send(err); }
 });
 
 var port = process.env.PORT || 80;
