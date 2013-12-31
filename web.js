b0c97471562eeaf3c3ddc4fe2737b93ad334f271
EN10 <sfm.neo@gmail.com>
2013-12-31 20:47:00 +0000

    Update web.js

--- web.js
+++ web.js
@@ -5,11 +5,15 @@ app.use(express.logger());
 app.get('/', function(req, res) {
   var fs = require('fs');
   try
-  { if (req.query.un != undefined && req.query.s != undefined)
+  { if (req.query.un !== undefined && req.query.s !== undefined)
     { fs.appendFileSync('Scores.txt', "<br>" + req.query.un + " " + req.query.s); }
-    // res.send(fs.readFileSync('Scores.txt').toString());
-    var array = fs.readFileSync('Scores.txt').toString().split(" ");
-    res.send(array);
+    res.send(fs.readFileSync('Scores.txt').toString());
+    /* var arr = fs.readFileSync('Scores.txt').toString().split(" ");
+    arr = arr.sort(function(a,b) {
+    return a[1] > b[1];
+    });
+    res.send(arr.toString);
+    */
   }
   catch(err)
   { res.send(err); }
