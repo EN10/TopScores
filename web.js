9cb63e57cc8686982aa027d4194053bd5b0ae680
EN10 <sfm.neo@gmail.com>
2013-12-31 03:56:42 +0000

    Update web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
   var fs = require('fs');
   try
   { if !(fs.exists('Scores.txt'))
-    { fs.openSync('Scores.txt', a+); }
+    { fs.openSync('Scores.txt', 'a+'); }
     
     if (req.query.q != undefined)
     { fs.appendFileSync('Scores.txt', "<br>" + req.query.q); }
