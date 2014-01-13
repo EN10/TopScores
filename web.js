fc1a50d4891f84bb256c4e28476b30a7171562b6
Enio <sfm.neo@gmail.com>
2014-01-13 05:08:54 -0500

    append + some sort

--- web.js
+++ web.js
@@ -1,32 +1,32 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
 app.get('/', function(req, res) {
   var fs = require('fs');
   try       // Insert Score
   {   var arr = fs.readFileSync('Scores.txt').toString().split(",");
       if (req.query.un !== undefined && req.query.s !== undefined)
-        { var scores = "";
-            for (var i=0; i<arr.length+2; i=i+2)
-            {   if (req.query.s <= arr[i+1])
-                { scores = scores +req.query.un +"," +arr[i+1] +","; }
+        {/*   var scores = "";    var insert = 0;
+            for (var i=0; i<arr.length; i=i+2)
+            {   if (req.query.s >= arr[i+1] && insert === 0)
+                { scores = scores +req.query.un +"," +req.query.s +",";
+                  scores = scores +arr[i] +"," +arr[i+1] +",";
+                  insert = 1; }
                 else
-                { scores = scores +arr[i] +"," +arr[i+1] +","; }
-            fs.writeFileSync('Scores.txt', scores); 
-            }
-        }
-            // Read & Output to HTML
+                { scores = scores +arr[i] +"," +arr[i+1] +","; } 
+            } 
+        fs.writeFileSync('Scores.txt', scores);*/
+        fs.appendFileSync('Scores.txt', "," + req.query.un + "," + req.query.s);
+        }   // Read & Output to HTML
+        arr = fs.readFileSync('Scores.txt').toString().split(",");
         var body = "";
-        for (var j=0; j<arr.length; j=i+2)
+        for (var j=0; j<arr.length-1; j=j+2)
         {   body = body + arr[j] +" " +arr[j+1] +"<br>";   }
         var head = '<a href="http://reaction.herokuapp.com/">Back to Reaction</a><p>';
-        res.send(head+body);
-  }
-  catch(err)
+        res.send(head+body); 
+  } catch(err)
   { res.send(err); }
 });
-
 var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
